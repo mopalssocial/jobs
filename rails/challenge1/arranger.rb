5fa47d10dad654f52665992c4f6604d403135e1a
ChrisSandison <chris.sandison@gmail.com>
2016-04-18 14:09:09 -0400

    How did that typo get there

--- rails/challenge1/arranger.rb
+++ rails/challenge1/arranger.rb
@@ -2,7 +2,7 @@ class Arranger
   class << self
     # Arrange str such that all the words are arranged in non-descending order
     # For example, arrange_string("Where sharing is rewarding") -> "is Where sharing rewarding"
-    def arrange_string(str)stno1
+    def arrange_string(str)
       
 
     end
